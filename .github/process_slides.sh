434d7537c99f43c57cb50e71a75ee269cc6585c0
Charlotte Curtis <ccurtis@mtroyal.ca>
2026-08-25 07:46:18 +0200

    Only process slides with changes

--- .github/process_slides.sh
+++ .github/process_slides.sh
@@ -2,6 +2,16 @@
 for md in lectures/*.md; do
     if ! grep -q "draft:\s*true" $md; then
         doc=$(basename "$md" .md)
+
+        # only process if the markdown is newer than the pdf
+        if [ -e lectures/pdfs/$doc.pdf ]; then
+            md_date=$(git log -1 --format=%ct $md)
+            pdf_date=$(git log -1 --format=%ct lectures/pdfs/$doc.pdf)
+            
+            if [ $md_date -lt $pdf_date ]; then
+                continue;
+            fi
+        fi
         npx @marp-team/marp-cli@latest --theme lectures/marp-mru.css --allow-local-files --pdf --html $md -o lectures/pdfs/$doc.pdf
         npx @marp-team/marp-cli@latest --theme lectures/marp-mru.css --allow-local-files --bespoke.progress --html $md -o lectures/html/$doc.html
 
@@ -10,7 +20,6 @@ for md in lectures/*.md; do
         
         # Add the relevant links to the main README page
         if ! grep -q "$title" README.md; then
-
             echo "| $date | [$title]($md) | <a href=\"lectures/pdfs/$doc.pdf\"><img src=\"img/file-pdf.svg\" alt=\"PDF\" style=\"height: 20px; margin: 0;\"/></a> |" >> README.md
         fi
     fi
